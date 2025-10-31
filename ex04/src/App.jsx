95fa00bdab3c6adc2bcfa1effaf4021abbb198fc
unknown <sramirezcarlosama@gmail.com>
2025-10-31 09:45:57 -0500

    Subi nuevos archivos o ejercicios enumerados

---
+++ ex04/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+    <div style={{display:'flex', boxShadow:'0 4 20px 20px  rgba(0, 0, 0, 0.2)'}}><h1 style={{color:'red', display:'flex', justifyContent:'center'}}>Hello world 🌎</h1></div>
+    
+    </>
+  )
+}
+ 
