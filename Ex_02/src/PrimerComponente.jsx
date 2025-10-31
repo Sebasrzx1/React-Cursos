c0030bc1b3e5fb3fd9838fefcc9a83beb76cd842
unknown <sramirezcarlosama@gmail.com>
2025-10-31 11:12:15 -0500

    Complete los ejercicio 1 2 y 3

---
+++ Ex_02/src/PrimerComponente.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const PrimerCompenente = () => {
+    return (
+        <div>Hello world como componente</div>
+    )
+}
---
+++ Ex_03/src/components/AgregarTarea.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useState } from "react"
+
+export const AgregarTarea = () => {
+    return(
+        <div></div>
+    )
+}
